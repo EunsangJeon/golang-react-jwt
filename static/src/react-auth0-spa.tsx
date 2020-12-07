eec1ea0de50627332b0cbbfce99095285154df95
unknown <eunsang.jeon@iam.samsungcloud.tv>
2020-12-07 23:54:25 +0000

    boilerplate

--- static/src/react-auth0-spa.tsx
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect, useContext } from 'react';
-import createAuth0Client from '@auth0/auth0-spa-js';
-
-const DEFAULT_REDIRECT_CALLBACK = () => {
-  window.history.replaceState({}, document.title, window.location.pathname);
-};
-
-export const Auth0Context = React.createContext();
