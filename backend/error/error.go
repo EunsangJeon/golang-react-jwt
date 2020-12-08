15ef0b375821e6161770f9220543d6599137ba63
unknown <eunsang.jeon@iam.samsungcloud.tv>
2020-12-08 03:36:20 +0000

    error and util package

---
+++ backend/error/error.go
@@ -0,0 +1,14 @@
+package error
+
+import "github.com/gin-gonic/gin"
+
+// ValidationErrors Users input validation errros
+var ValidationErrors = []string{}
+
+//HandleErr is generic error handler, logs error and Os.Exit(1)
+func HandleErr(c *gin.Context, err error) error {
+	if err != nil {
+		c.Error(err)
+	}
+	return err
+}
---
+++ backend/util/util.go
@@ -0,0 +1,38 @@
+package util
+
+import (
+	"regexp"
+
+	"github.com/EunsangJeon/golang-react-jwt/backend/db"
+)
+
+const (
+	emailRegex = `^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,4}$`
+)
+
+// ValidateUser returns a slice of string of validation errors
+func ValidateUser(user db.Register, err []string) []string {
+	emailCheck := regexp.MustCompile(emailRegex).MatchString(user.Email)
+	if emailCheck != true {
+		err = append(err, "Invalid email")
+	}
+	if len(user.Password) < 4 {
+		err = append(err, "Invalid password, Password should be more than 4 characters")
+	}
+	if len(user.Name) < 1 {
+		err = append(err, "Invalid name, please enter a name")
+	}
+
+	return err
+}
+
+// ValidatePasswordReset returns boolean to show if it passed the test and string if not passed.
+func ValidatePasswordReset(resetPassword db.ResetPassword) (bool, string) {
+	if len(resetPassword.Password) < 4 {
+		return false, "Invalid password, password should be more than 4 characters"
+	}
+	if resetPassword.Password != resetPassword.ConfirmPassword {
+		return false, "Password reset failed, passwords must match"
+	}
+	return true, ""
+}
