2d08710ecc8e7f9409a0d80ca4f84ce3208169e7
unknown <eunsang.jeon@iam.samsungcloud.tv>
2020-12-10 07:26:04 +0000

    controller done

--- backend/controller/controller.go
+++ backend/controller/controller.go
@@ -1,8 +1,10 @@
 package controller
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/EunsangJeon/golang-react-jwt/backend/config"
 	"github.com/EunsangJeon/golang-react-jwt/backend/db"
@@ -79,4 +81,90 @@ func ResetPassword(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusOK, gin.H{"success": false, "errors": errStr})
 	}
+}
+
+// Create new user
+func Create(c *gin.Context) {
+	var user db.Register
+	c.Bind(&user)
+	exists := checkUserExists(user)
+
+	valErr := util.ValidateUser(user, errors.ValidationErrors)
+	if exists == true {
+		valErr = append(valErr, "email already exists")
+	}
+
+	fmt.Println(valErr)
+	if len(valErr) > 0 {
+		c.JSON(http.StatusUnprocessableEntity, gin.H{"success": false, "errors": valErr})
+		return
+	}
+	db.HashPassword(&user)
+	_, err := db.DB.Query(db.CreateUserQuery, user.Name, user.Password, user.Email)
+	errors.HandleErr(c, err)
+	c.JSON(http.StatusOK, gin.H{"success": true, "msg": "User created succesfully"})
+}
+
+// Session returns JSON of user info
+func Session(c *gin.Context) {
+	user, isAuthenticated := AuthMiddleware(c, jwtKey)
+	if !isAuthenticated {
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "msg": "unauthorized"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"success": true, "user": user})
+}
+
+// Login controller
+func Login(c *gin.Context) {
+	var user db.Login
+	c.Bind(&user)
+
+	row := db.DB.QueryRow(db.LoginQuery, user.Email)
+
+	var id int
+	var name, email, password, createdAt, updatedAt string
+
+	err := row.Scan(&id, &name, &password, &email, &createdAt, &updatedAt)
+
+	if err == sql.ErrNoRows {
+		fmt.Println(sql.ErrNoRows, "err")
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "msg": "incorrect credentials"})
+		return
+	}
+
+	match := db.CheckPasswordHash(user.Password, password)
+	if !match {
+		c.JSON(http.StatusUnauthorized, gin.H{"success": false, "msg": "incorrect credentials"})
+		return
+	}
+
+	//expiration time of the token ->30 mins
+	expirationTime := time.Now().Add(30 * time.Minute)
+
+	// Create the JWT claims, which includes the User struct and expiry time
+	claims := &Claims{
+		User: db.User{
+			Name: name, Email: email, CreatedAt: createdAt, UpdatedAt: updatedAt,
+		},
+		StandardClaims: jwt.StandardClaims{
+			//expiry time, expressed as unix milliseconds
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT token string
+	tokenString, err := token.SignedString(jwtKey)
+	errors.HandleErr(c, err)
+	// c.SetCookie("token", tokenString, expirationTime, "", "*", true, false)
+	http.SetCookie(c.Writer, &http.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+
+	fmt.Println(tokenString)
+	c.JSON(http.StatusOK, gin.H{"success": true, "msg": "logged in succesfully", "user": claims.User, "token": tokenString})
 }
