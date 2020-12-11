016897a8b5f5a5975a5b466def6b723447032400
unknown <eunsang.jeon@iam.samsungcloud.tv>
2020-12-11 08:00:00 +0000

    Login frontend

---
+++ frontend/src/components/Login.tsx
@@ -0,0 +1,105 @@
+import { FC, useState } from 'react';
+import { History } from 'history';
+import { apiURl } from '../api';
+import { createCookie } from '../utils';
+import { User } from '../models/User';
+
+interface loginProps {
+  history: History;
+}
+
+interface handleChangeEvent {
+  target: {
+    name: string;
+    value: string;
+  };
+}
+
+interface loginResponse {
+  token: string;
+  success: boolean;
+  msg: string;
+  user: User;
+}
+
+export const Login: FC<loginProps> = ({ history }) => {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+    isSubmitting: false,
+    message: '',
+  });
+
+  const { email, password, isSubmitting, message } = state;
+
+  const handleChange = async (event: handleChangeEvent) => {
+    const { name, value } = event.target;
+    await setState({ ...state, [name]: value });
+  };
+
+  const handleSubmit = async () => {
+    setState({ ...state, isSubmitting: true });
+
+    const { email, password } = state;
+    try {
+      const res: loginResponse = await fetch(`${apiURl}/login`, {
+        method: 'POST',
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).then((res) => res.json());
+
+      const { token, success, msg, user } = res;
+
+      if (!success) {
+        return setState({
+          ...state,
+          message: msg,
+          isSubmitting: false,
+        });
+      }
+      // expire in 30 minutes(same time as the cookie is invalidated on the backend)
+      createCookie('token', token, 0.5);
+
+      history.push({ pathname: '/session', state: user });
+    } catch (error) {
+      setState({ ...state, message: error.toString(), isSubmitting: false });
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <h1>Login</h1>
+      <input
+        className="input"
+        type="text"
+        placeholder="email"
+        value={email}
+        name="email"
+        onChange={(event) => {
+          handleChange(event);
+        }}
+      />
+
+      <input
+        className="input"
+        type="password"
+        placeholder="password"
+        value={password}
+        name="password"
+        onChange={(event) => {
+          handleChange(event);
+        }}
+      />
+
+      <button disabled={isSubmitting} onClick={() => handleSubmit()}>
+        {isSubmitting ? '.....' : 'login'}
+      </button>
+      <div className="message">{message}</div>
+    </div>
+  );
+};
---
+++ frontend/src/models/User.ts
@@ -0,0 +1,7 @@
+export interface User {
+  pasword: string;
+  email: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
