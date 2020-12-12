024770a84af635bd662d25ffae36bd2431eb9945
Eunsang <nicolaus1207@gmail.com>
2020-12-12 19:40:37 +0900

    eslint fix

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -2,17 +2,17 @@ export const createCookie = (
   cookieName: string,
   cookieValue: string,
   minuteToExpire: number
-) => {
+): void => {
   const date = new Date();
   date.setTime(date.getTime() + minuteToExpire * 60 * 1000);
   document.cookie = `${cookieName} = ${cookieValue}; expires = ${date.toUTCString()}`;
 };
 
-export const checkCookie = (name: string) => {
+export const checkCookie = (name: string): string => {
   console.log(name);
   return document.cookie;
 };
 
-export const deleteCookie = (name: string) => {
+export const deleteCookie = (name: string): void => {
   document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 };
